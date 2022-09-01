b2e859a71b351187f3afcb977e055bce16439d0b
dovela <892117745@qq.com>
2022-09-02 01:00:10 +0800

    更新

---
+++ src/views/sakana-widget.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    new SakanaWidget().mount('#sakana-widget');
+  </div>
+</template>
+
+<script>
+import SakanaWidget from 'sakana-widget'
+</script>
+
+<style scoped>
+
+</style>
